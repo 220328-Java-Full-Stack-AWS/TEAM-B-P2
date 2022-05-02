c6d6aa868fbf5d61d0719519b897a43ea63ccf0d
Kenn1690 <Kenneth@Strok>
2022-05-02 18:30:34 -0400

    This is a test schema and will not be actually implemented. It is not implemented because hibernate will be making the schema for us.

---
+++ P2TestScripts/productCreation.sql
@@ -0,0 +1,7 @@
+select * from category;
+select * from products;
+
+insert into products (product_name, product_price, product_description, product_category)
+values('Golf club', 199.99, 'Driver', 1);
+insert into products (product_name, product_price, product_description, product_category)
+values('Soccer ball', 29.99, 'Soccer ball, multicolored', 1);
---
+++ P2TestScripts/userCreation.sql
@@ -0,0 +1,4 @@
+insert into users (username, user_password, user_first_name, user_last_name, user_email)
+values('kenn1690', 'password', 'kenneth', 'strohm', 'ks@gmail.com');
+
+select * from users;
