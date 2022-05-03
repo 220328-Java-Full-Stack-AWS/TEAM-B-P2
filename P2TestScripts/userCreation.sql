1c9f32c769b4af0cb46311d36f63c90a574da4df
Simmons Banana <simmons@thinkful.com>
2022-05-03 12:16:01 -0700

    maven startup barebone

--- P2TestScripts/userCreation.sql
+++
@@ -1,4 +0,0 @@
-insert into users (username, user_password, user_first_name, user_last_name, user_email)
-values('kenn1690', 'password', 'kenneth', 'strohm', 'ks@gmail.com');
-
-select * from users;
