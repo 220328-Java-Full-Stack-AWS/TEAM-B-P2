153b660bc7f82d3e0050a3e51d86a534ec5ad004
seaboyz <seaboyz@hotmail.com>
2022-05-02 23:38:38 -0500

    Rename reigister component to sign-in-sign-up

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterUserComponent } from './register-user/register-user.component';
+import { RegisterUserComponent } from './sign-in-sign-up/register-user.component';
 import { FormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 import { MatCardModule } from '@angular/material/card';
