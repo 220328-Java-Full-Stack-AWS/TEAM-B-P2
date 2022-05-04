6bc430ca3758fe1c1cbb3a4c77617ede9962a88a
seaboyz <seaboyz@hotmail.com>
2022-05-04 08:37:28 -0500

    Use event binding to pass username and password to model

--- frontend/src/app/sign-in-sign-up/sign-in-sign-up.component.ts
+++ frontend/src/app/sign-in-sign-up/sign-in-sign-up.component.ts
@@ -7,11 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInSignUpComponent implements OnInit
 {
+  #email: string;
+  #password: string;
 
-  constructor() { }
+  constructor()
+  {
+    this.#email = "";
+    this.#password = "";
+  }
 
   ngOnInit(): void
   {
+  }
+
+  onEmailChange(event: Event): void
+  {
+    this.#email = (<HTMLInputElement>event.target).value;
+    console.log(this.#email);
+  }
+
+  onPasswordChange(event: Event): void
+  {
+    this.#password = (<HTMLInputElement>event.target).value;
+    console.log(this.#password);
   }
 
 }
