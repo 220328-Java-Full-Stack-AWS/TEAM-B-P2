876493863789d75a25b63918523efd5d0db22ab8
Kenn1690 <Kenneth@Strok>
2022-05-11 23:23:37 -0400

    need to make order item services

--- p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
+++ p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
@@ -1,4 +1,10 @@
 package com.revature.p2backend.beans.services;
 
 public class OrderItemService {
+    //TODO convert this into a service and add appropriate methods
+    /*
+    examples of methods: create, update, view
+    Keep in mind, OrderItem does have a relationship.
+    Please feel free to create OrderItemDao methods if needed.
+     */
 }
