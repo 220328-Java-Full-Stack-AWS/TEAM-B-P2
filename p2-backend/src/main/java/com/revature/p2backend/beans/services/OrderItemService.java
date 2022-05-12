5a496afd719761accdc1df53fc2d21a4752b37e2
Kenn1690 <Kenneth@Strok>
2022-05-11 23:30:23 -0400

    added a few more comments

--- p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
+++ p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
@@ -5,6 +5,10 @@ public class OrderItemService {
     /*
     examples of methods: create, update, view
     Keep in mind, OrderItem does have a relationship.
+    Also, for each product received by the front end, a new
+    order item will need to be created. We need to find a
+    way to send back the quantity as well.
     Please feel free to create OrderItemDao methods if needed.
+
      */
 }
