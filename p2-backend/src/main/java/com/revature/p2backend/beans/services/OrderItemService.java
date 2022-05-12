0453b7044a5cddbc4aaacea456327aeaaaf2121a
Kenn1690 <Kenneth@Strok>
2022-05-11 23:28:23 -0400

    need to create orders service

--- p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
+++ p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
@@ -1,10 +1,5 @@
 package com.revature.p2backend.beans.services;
 
 public class OrderItemService {
-    //TODO convert this into a service and add appropriate methods
-    /*
-    examples of methods: create, update, view
-    Keep in mind, OrderItem does have a relationship.
-    Please feel free to create OrderItemDao methods if needed.
-     */
+
 }
--- p2-backend/src/main/java/com/revature/p2backend/beans/services/OrdersService.java
+++ p2-backend/src/main/java/com/revature/p2backend/beans/services/OrdersService.java
@@ -1,4 +1,12 @@
 package com.revature.p2backend.beans.services;
 
 public class OrdersService {
+    //TODO convert this into a service and add appropriate methods
+    /*
+    examples of methods: create, view
+    Keep in mind, Orders does have a relationships.
+    Please feel free to create OrderItemDao methods if needed.
+    Also, we should think if we just want a new order created
+    everytime a user logs in.
+     */
 }
