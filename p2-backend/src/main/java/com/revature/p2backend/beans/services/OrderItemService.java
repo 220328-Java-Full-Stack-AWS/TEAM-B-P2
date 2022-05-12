2171a209bdbd66485f389db3b2033f7080ed4141
Kenn1690 <Kenneth@Strok>
2022-05-11 23:33:33 -0400

    need to make product services

--- p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
+++ p2-backend/src/main/java/com/revature/p2backend/beans/services/OrderItemService.java
@@ -1,14 +1,4 @@
 package com.revature.p2backend.beans.services;
 
 public class OrderItemService {
-    //TODO convert this into a service and add appropriate methods
-    /*
-    examples of methods: create, update, view
-    Keep in mind, OrderItem does have a relationship.
-    Also, for each product received by the front end, a new
-    order item will need to be created. We need to find a
-    way to send back the quantity as well.
-    Please feel free to create OrderItemDao methods if needed.
-
-     */
 }
--- p2-backend/src/main/java/com/revature/p2backend/beans/services/ProductService.java
+++ p2-backend/src/main/java/com/revature/p2backend/beans/services/ProductService.java
@@ -1,4 +1,12 @@
 package com.revature.p2backend.beans.services;
 
 public class ProductService {
+    //TODO convert this into a service and add appropriate methods
+    /*
+    examples of methods: view
+    really the user should not be able to do anything
+    with the product besides view and add to cart. Adding
+    to cart will be handled by front.
+
+     */
 }
